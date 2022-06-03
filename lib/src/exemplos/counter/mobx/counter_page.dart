6026f306e0d6bca8768a028877ec5524df6b1081
Bruno Lima <brunoblsouza@icloud.com>
2022-06-03 17:58:40 -0300

    Exemplo com mobX

---
+++ lib/src/exemplos/counter/mobx/counter_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import 'counter.dart';
+
+class ContadorMobx extends StatefulWidget {
+  const ContadorMobx({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<ContadorMobx> createState() => _ContadorContadorMobx();
+}
+
+class _ContadorContadorMobx extends State<ContadorMobx> {
+  final _controller = Counter();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('You have pushed the button this many times:'),
+            Observer(),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _controller.increment,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
